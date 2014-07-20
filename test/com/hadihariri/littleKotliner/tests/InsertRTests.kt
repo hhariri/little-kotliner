84738442dab758cccfb0ab68b636ab789d8ad5cd
Hadi Hariri <mail@hadihariri.com>
2014-07-20 12:28:56 +0200

    About to implement insertR

---
+++ test/com/hadihariri/littleKotliner/tests/InsertRTests.kt
@@ -0,0 +1,11 @@
+package com.hadihariri.littleKotliner.tests
+
+import org.junit.Test
+import com.hadihariri.littleKotliner.*
+
+public class InsertRTests {
+
+    Test fun insertRNewOldOnListWillInsertNew() {
+        throw Exception("left off here")
+    }
+}
